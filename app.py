638cb57be2a62589e40d34bee936c4ee0ac83cb1
Gargamel-l <belyton2002@yandex.ru>
2024-03-01 21:48:26 +0300

    Add project files.

---
+++ app.py
@@ -0,0 +1,267 @@
+﻿from calendar import c
+import os
+import psycopg2
+from psycopg2.extensions import register_type, new_array_type
+from flask import Flask, render_template, request, url_for, redirect, flash, session
+from config import host, user, password, db_name, db_name2
+import re 
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = 'A.S.Belyaev BSBO-07-20'
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        user=user,
+        password=password,
+        host=host,
+        database=db_name
+        )
+    return conn
+
+def get_db_connection2():
+    conn2 = psycopg2.connect(
+        user=user,
+        password=password,
+        host=host,
+        database=db_name2
+        )
+    return conn2
+
+@app.route("/")
+def index():
+    if 'loggedin' in session:
+        return render_template('profile.html', username=session['login'])
+    return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('auth.html')
+
+@app.route('/profile')
+def profile():
+    user_id = session['id'];
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users WHERE id = %s', (user_id,))
+    users = cur.fetchone()
+    cur.execute('SELECT books_id FROM users WHERE id = %s', (user_id,))
+    myBook = cur.fetchone()
+    cur.close()
+    conn.close()
+    if int(myBook[0]) > 0:
+            conn2 = get_db_connection2()
+            cur = conn2.cursor()
+            cur.execute('SELECT * FROM books WHERE id = %s', (myBook,))
+            books = cur.fetchone()
+            cur.close()
+            conn2.close()
+            return render_template('profile.html', show_hidden=True, users = users, books = books)
+    else:
+            return render_template('profile.html', show_hidden=False, users = users)
+
+
+@app.route('/signUp',methods=['GET', 'POST'])
+def signUp():
+    if request.method == 'POST':
+        login = request.form['login']
+        email = request.form['email']
+        password = request.form['password']
+        _hashed_password = generate_password_hash(password)
+            
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        cur.execute('SELECT * FROM users WHERE login = %s', (login,))
+        account = cur.fetchone() 
+        if account:
+            flash('Аккаунт уже существует!')
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            flash('Недопустимый адрес электронной почты!')
+        elif not re.match(r'[A-Za-z0-9]+', login):
+            flash('Логин может содержать только буквы и цифры')
+        elif not login or not password or not email:
+            flash('Заполните форму!')
+        else:
+            cur.execute('INSERT INTO users (login, email, password,status,books_id,books_count)'
+                        'VALUES (%s, %s, %s, %s, %s, %s)',
+                        (login, email, _hashed_password, 'Читатель', 0, 0))
+            cur.execute('SELECT * FROM users WHERE login = %s', (login,))
+            account = cur.fetchone() 
+            session['loggedin'] = True
+            session['id'] = account[0]
+            session['login'] = account[1]
+            conn.commit()
+            cur.close()
+            conn.close()
+            return redirect(url_for('profile'))
+    return render_template('auth.html')
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'login' in request.form and 'password' in request.form:
+        login = request.form['login']
+        password = request.form['password']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM users WHERE login = %s', (login,))
+        account = cur.fetchone() 
+  
+        
+        if account:
+            password_rs = account[3]
+            if check_password_hash(password_rs, password):
+                session['loggedin'] = True
+                session['id'] = account[0]
+                session['login'] = account[1]
+                return redirect(url_for('profile'))
+            else:
+                flash('inncorrect password')
+        else:
+            flash('inncorrect username')
+    
+        conn.commit()
+        cur.close()
+        conn.close()
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   return redirect(url_for('index'))
+
+@app.route('/donation/', methods=('GET', 'POST'))
+def donation():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        pages_num = int(request.form['pages_num'])
+        review = request.form['review']
+
+        conn2 = get_db_connection2()
+        cur = conn2.cursor()
+        cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                    'VALUES (%s, %s, %s, %s)',
+                    (title, author, pages_num, review))
+        conn2.commit()
+        cur.close()
+        conn2.close()
+
+        user_id = session['id'];
+        new_status = 'Щедрая душа';
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('UPDATE users set status = %s WHERE id = %s', (new_status ,user_id),)
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return redirect(url_for('showcaseBooks'))
+    return render_template('donation.html')
+
+@app.route("/addToList", methods=('GET', 'POST'))
+def addToList():
+
+    if request.method == 'POST':
+        user_id = session['id'];
+        books_id = request.form['book_id']
+        books_genre = request.form['book_id']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('UPDATE users set books_id = %s WHERE id = %s', (books_id ,user_id),)
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('profile'))
+    return render_template('showcaseBooks.html')
+
+@app.route("/readenBooks")
+def readenBooks():
+        user_id = session['id'];
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT books_count FROM users WHERE id = %s', (user_id,))
+        books_count = cur.fetchone()
+        cur.execute('SELECT books_id FROM users WHERE id = %s', (user_id,))
+        readenBook_id = cur.fetchone()
+        conn.commit()
+        cur.close()
+        conn.close()
+        if int(books_count[0]) > 0:
+            conn2 = get_db_connection2()
+            cur = conn2.cursor()
+            cur.execute('SELECT * FROM books WHERE id = %s', (readenBook_id,))
+            books = cur.fetchone()
+            cur.close()
+            conn2.close()
+            return render_template('readenBooks.html', show_hidden=True,  books = books, books_count = books_count)
+        else:
+            return render_template('readenBooks.html', show_hidden=False)
+        return render_template('readenBooks.html')
+
+@app.route("/passTheBook")
+def passTheBook():
+        user_id = session['id'];
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT books_id FROM users WHERE id = %s', (user_id,))
+        readenBook_id = cur.fetchone()
+        if int(readenBook_id[0]) > 0:
+            cur.execute('UPDATE users set books_count = books_count + 1 WHERE id = %s', (user_id,))
+            cur.execute('UPDATE users set books_id = %s WHERE id = %s', (0,user_id),)
+        cur.execute('SELECT books_count FROM users WHERE id = %s', (user_id,))
+        books_count = cur.fetchone()
+        conn.commit()
+        cur.close()
+        conn.close()
+        conn2 = get_db_connection2()
+        cur = conn2.cursor()
+        cur.execute('SELECT * FROM books WHERE id = %s', (readenBook_id,))
+        books = cur.fetchone()
+        cur.close()
+        conn2.close()
+        if int(books_count[0]) > 0:
+            return render_template('readenBooks.html', show_hidden=True,  books = books, books_count = books_count)
+        else:
+            return render_template('readenBooks.html', show_hidden=False)
+
+
+@app.route("/chooseLibrary")
+def chooseLibrary():
+    conn2 = get_db_connection2()
+    cur = conn2.cursor()
+    cur.execute('SELECT * FROM librarys')
+    librarys = cur.fetchall()
+    cur.close()
+    conn2.close()
+    return render_template('chooseLibrary.html', librarys = librarys)
+
+@app.route('/showcaseBooks')
+def showcaseBooks():
+    conn2 = get_db_connection2()
+    cur = conn2.cursor()
+    cur.execute('SELECT * FROM books')
+    books = cur.fetchall()
+    cur.close()
+    conn2.close()
+    return render_template('showcaseBooks.html', books=books)
+
+@app.route('/RSL_Books')
+def RSL_Books():
+    conn2 = get_db_connection2()
+    cur = conn2.cursor()
+    cur.execute('SELECT * FROM rsl_bookscomedy')
+    book_comedy = cur.fetchall()
+    cur.execute('SELECT * FROM rsl_bookshorror')
+    book_horror = cur.fetchall()
+    cur.execute('SELECT * FROM rsl_booksfantasy')
+    book_fantasy = cur.fetchall()
+    cur.close()
+    conn2.close()
+    return render_template('RSL_Books.html', book_comedy=book_comedy, book_horror=book_horror, book_fantasy=book_fantasy)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ database.py
@@ -0,0 +1,413 @@
+﻿import os
+import random
+import string
+from pickle import FALSE
+import psycopg2
+from config import host, user, password, db_name, db_name2
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+
+
+try:
+    conn = psycopg2.connect(
+        user=user,
+        password=password,
+        host=host,
+        database=db_name)
+
+    conn.autocommit = True
+
+    cur = conn.cursor()
+
+    cur.execute('DROP TABLE IF EXISTS users;')
+    hash = generate_password_hash('1234')
+    print(hash)
+    input('next')
+
+    with conn.cursor() as cursor:
+        cur.execute(
+             """CREATE TABLE users(id serial PRIMARY KEY,
+                                 login varchar (150) NOT NULL,
+                                 email varchar (150) NOT NULL,
+                                 password varchar (800) NOT NULL,
+                                 status varchar (150) NOT NULL,
+                                 books_id integer NOT NULL,
+                                 books_count integer NOT NULL);"""
+        )
+    
+
+    cur.execute('INSERT INTO users (login, email, password, status, books_id, books_count)'
+            'VALUES (%s, %s, %s, %s, %s, %s)',
+             ('Anton',
+             'anton@yandex.ru',
+             'pbkdf2:sha256:260000$gGT19AjqXD37PW2S$93e4ed7b8e2aafea10302da9b6dfa87aa95dd780e81018f6d7e491d4e014ce40',
+             'admin',
+             '0',
+             '0')
+            )
+    print("[INFO] succses")
+
+    with conn.cursor() as cursor:
+        cur.execute('DROP TABLE IF EXISTS top_users;')
+        cur.execute(
+             """CREATE TABLE top_users(id serial PRIMARY KEY,
+                                 login varchar (150) NULL,
+                                 place integer NULL,
+                                 books_count integer NULL);"""
+        )
+    cur.execute('DROP TABLE IF EXISTS top_donators;')
+    cur.execute(
+             """CREATE TABLE top_donators(id serial PRIMARY KEY,
+                                 login varchar (150) NULL,
+                                 place integer NULL,
+                                 donate_count integer NULL);"""
+        )
+    
+
+except Exception as _ex:
+    print("[INFO] Error", _ex)
+
+finally:
+        if conn:
+            conn.close()
+            print()
+
+try:
+
+    conn2 = psycopg2.connect(
+        user=user,
+        password=password,
+        host=host,
+        database=db_name2)
+
+    conn2.autocommit = True
+
+    cur = conn2.cursor()
+
+
+    cur.execute('DROP TABLE IF EXISTS librarys;')
+    cur.execute(
+        """CREATE TABLE librarys (id serial PRIMARY KEY,
+                                 name varchar (150) NOT NULL,
+                                 reputation integer NOT NULL,
+                                 adress varchar (250) NOT NULL);"""
+                )
+    cur.execute('INSERT INTO librarys (name, reputation, adress)'
+                'VALUES (%s, %s, %s)',
+               ('Российская государственная библиотека',
+               5,
+              'ул. Воздвиженка, 3/5, Москва')
+             )
+    cur.execute('INSERT INTO librarys (name, reputation, adress)'
+                'VALUES (%s, %s, %s)',
+               ('Дом Пашкова',
+               5,
+              'ул. Воздвиженка, 3/5с1, Москва')
+             )
+    cur.execute('INSERT INTO librarys (name, reputation, adress)'
+                'VALUES (%s, %s, %s)',
+               ('Библиотека-читальня им. И.С. Тургенева',
+               5,
+              'Бобров пер., 6, стр. 1, Москва')
+             )
+
+
+    cur.execute('DROP TABLE IF EXISTS books;')
+    cur.execute(
+        """CREATE TABLE books (id serial PRIMARY KEY,
+                                 title varchar (150) NOT NULL,
+                                 author varchar (150) NOT NULL,
+                                 pages_num varchar (50) NOT NULL,
+                                 review text,
+                                 date_added date DEFAULT CURRENT_TIMESTAMP);"""
+                )
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+               )
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+               )
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!'))
+
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
+             'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!'))
+
+    def populate_table():
+            title = ''.join(random.choice(string.ascii_letters) for _ in range(10))
+            author = ''.join(random.choice(string.ascii_letters) for _ in range(10))
+            pages_num = str(random.randint(100, 1000))
+            review = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(50))
+
+            cur.execute("INSERT INTO books (title, author, pages_num, review) VALUES (%s, %s, %s, %s)",
+                    (title, author, pages_num, review))
+    for _ in range(500):
+        populate_table()
+    
+    input('next')
+
+    print("[INFO] succsesssssssssssssssssssssss")
+    
+
+
+    ###Russian state library
+    cur.execute('DROP TABLE IF EXISTS RSL_BooksHorror;')
+    cur.execute(
+        """CREATE TABLE RSL_BooksHorror (id serial PRIMARY KEY,
+                                 title varchar (150) NOT NULL,
+                                 author varchar (150) NOT NULL,
+                                 pages_num varchar (50) NOT NULL,
+                                 review text,
+                                 date_added date DEFAULT CURRENT_TIMESTAMP);"""
+                )
+    cur.execute('INSERT INTO RSL_BooksHorror (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO RSL_BooksHorror (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+)
+    cur.execute('DROP TABLE IF EXISTS RSL_BooksComedy;')
+    cur.execute(
+        """CREATE TABLE RSL_BooksComedy (id serial PRIMARY KEY,
+                                 title varchar (150) NOT NULL,
+                                 author varchar (150) NOT NULL,
+                                 pages_num varchar (50) NOT NULL,
+                                 review text,
+                                 date_added date DEFAULT CURRENT_TIMESTAMP);"""
+                )
+    cur.execute('INSERT INTO RSL_BooksComedy (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO RSL_BooksComedy (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+)
+    cur.execute('DROP TABLE IF EXISTS RSL_BooksFantasy;')
+    cur.execute(
+        """CREATE TABLE RSL_BooksFantasy (id serial PRIMARY KEY,
+                                 title varchar (150) NOT NULL,
+                                 author varchar (150) NOT NULL,
+                                 pages_num varchar (50) NOT NULL,
+                                 review text,
+                                 date_added date DEFAULT CURRENT_TIMESTAMP);"""
+                )
+    cur.execute('INSERT INTO RSL_BooksFantasy (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO RSL_BooksFantasy (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+)
+
+    #Peshkov house
+    cur.execute('DROP TABLE IF EXISTS PH_BooksHorror;')
+    cur.execute(
+        """CREATE TABLE PH_BooksHorror (id serial PRIMARY KEY,
+                                 title varchar (150) NOT NULL,
+                                 author varchar (150) NOT NULL,
+                                 pages_num varchar (50) NOT NULL,
+                                 review text,
+                                 date_added date DEFAULT CURRENT_TIMESTAMP);"""
+                )
+    cur.execute('INSERT INTO PH_BooksHorror (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO PH_BooksHorror (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+)
+    cur.execute('DROP TABLE IF EXISTS PH_BooksComedy;')
+    cur.execute(
+        """CREATE TABLE PH_BooksComedy (id serial PRIMARY KEY,
+                                 title varchar (150) NOT NULL,
+                                 author varchar (150) NOT NULL,
+                                 pages_num varchar (50) NOT NULL,
+                                 review text,
+                                 date_added date DEFAULT CURRENT_TIMESTAMP);"""
+    )
+    cur.execute('INSERT INTO PH_BooksComedy (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO PH_BooksComedy (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+)
+    cur.execute('DROP TABLE IF EXISTS PH_BooksFantasy;')
+    cur.execute(
+        """CREATE TABLE PH_BooksFantasy (id serial PRIMARY KEY,
+                                 title varchar (150) NOT NULL,
+                                 author varchar (150) NOT NULL,
+                                 pages_num varchar (50) NOT NULL,
+                                 review text,
+                                 date_added date DEFAULT CURRENT_TIMESTAMP);"""
+    )
+    cur.execute('INSERT INTO PH_BooksFantasy (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO PH_BooksFantasy (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+)
+
+    #Turgeniev library
+    cur.execute('DROP TABLE IF EXISTS TL_BooksHorror;')
+    cur.execute(
+        """CREATE TABLE TL_BooksHorror (id serial PRIMARY KEY,
+                                 title varchar (150) NOT NULL,
+                                 author varchar (150) NOT NULL,
+                                 pages_num varchar (50) NOT NULL,
+                                 review text,
+                                 date_added date DEFAULT CURRENT_TIMESTAMP);"""
+                )
+    cur.execute('INSERT INTO TL_BooksHorror (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO TL_BooksHorror (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+)
+    cur.execute('DROP TABLE IF EXISTS TL_BooksComedy;')
+    cur.execute(
+        """CREATE TABLE TL_BooksComedy (id serial PRIMARY KEY,
+                                 title varchar (150) NOT NULL,
+                                 author varchar (150) NOT NULL,
+                                 pages_num varchar (50) NOT NULL,
+                                 review text,
+                                 date_added date DEFAULT CURRENT_TIMESTAMP);"""
+                )
+    cur.execute('INSERT INTO TL_BooksComedy (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO TL_BooksComedy (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+)
+    cur.execute('DROP TABLE IF EXISTS TL_BooksFantasy;')
+    cur.execute(
+        """CREATE TABLE TL_BooksFantasy (id serial PRIMARY KEY,
+                                 title varchar (150) NOT NULL,
+                                 author varchar (150) NOT NULL,
+                                 pages_num varchar (50) NOT NULL,
+                                 review text,
+                                 date_added date DEFAULT CURRENT_TIMESTAMP);"""
+            )
+    cur.execute('INSERT INTO TL_BooksFantasy (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+               ('A Tale of Two Cities',
+                'Charles Dickens',
+               489,
+              'A great classic!')
+             )
+    cur.execute('INSERT INTO TL_BooksFantasy (title, author, pages_num, review)'
+               'VALUES (%s, %s, %s, %s)',
+              ('Anna Karenina',
+               'Leo Tolstoy',
+              864,
+               'Another great classic!')
+            )
+
+    cur.execute('DROP TABLE IF EXISTS TL_BooksFantasy;')
+    print("[INFO] succses")
+    
+except Exception as _ex1:
+    print("[INFO] Error", _ex1)
+
+finally:
+        if conn2:
+            conn2.close()
+            print()
